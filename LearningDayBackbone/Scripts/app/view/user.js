a78985d0a74cc2e6d7d1335318377fde6d17aa63
Victor Cabello <victorcabello@usa-intech.com>
2014-02-26 01:46:19 -0600

    add button to add user in the dom"

--- LearningDayBackbone/Scripts/app/view/user.js
+++ LearningDayBackbone/Scripts/app/view/user.js
@@ -2,16 +2,27 @@
 (function () {
     window.App.Views['User'] = Backbone.View.extend({
         el: $('body'), // attaches `this.el` to an existing element.
-
+        events: {
+            'click button#add': 'addItem'
+        },
         initialize: function () {
-            _.bindAll(this, 'render'); // fixes loss of context for 'this' within methods
+            _.bindAll(this, 'render', 'addItem'); // every function that uses 'this' as the current object should be in here
 
-            this.render(); // not all views are self-rendering. This one is.
+            this.counter = 0; // total number of items added thus far
+            this.render();
         },
 
         render: function () {
-            $(this.el).append("<ul> <li>hello world</li> </ul>");
+            $(this.el).append("<button id='add'>Add user</button>");
+            $(this.el).append("<ul></ul>");
+        },
+
+        addItem: function () {
+            this.counter++;
+            $('ul', this.el).append("<li>Adding user " + this.counter + "</li>");
         }
+        
+
     });
 
 })();
