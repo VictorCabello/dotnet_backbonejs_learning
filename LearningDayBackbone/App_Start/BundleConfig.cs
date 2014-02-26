7c1dd43fec6544cbe7a63c6dfd3219fec6d109ec
Victor Cabello <victorcabello@usa-intech.com>
2014-02-26 00:09:20 -0600

    Include web optimization to use backbonejs libraries in the layout

---
+++ LearningDayBackbone/App_Start/BundleConfig.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+namespace LearningDayBackbone
+{
+    public class BundleConfig
+    {
+
+        // For more information on Bundling, visit http://go.microsoft.com/fwlink/?LinkId=254725
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                "~/Scripts/jquery-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/backbone")
+                .Include("~/Scripts/underscore.js")
+                .Include("~/Scripts/Backbone.js"));
+
+        }
+    }
+}
